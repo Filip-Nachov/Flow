95c48dec6a54b1a009bcb8b93abbc843234d1218
Filip Nachov <fnachov@gmail.com>
2025-06-10 18:17:26 +0200

    FIX: MADE THE CODE WORK  CRAZY

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#include "headers/main.h"
+#include "headers/task.h"
 
 int main() {
     // TODO: Actually do the project and stop writing stupid messages
-    std::cout << "Filip is the greatest programmer!" << '\n';
 
     return 0;
 }
